d13bd6386cab9b8bc6a16797df1395d08bc33c0e
Patryk Sztuczka <73931963+patryksztuczka@users.noreply.github.com>
2025-01-25 16:51:43 +0100

    Feature/implement game board and player character (#1)
    * Added some configuration to prettier and tailwind css
    
    * Implemented basic board and player with move and crosshair handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,11 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Board from "./components/board";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  console.log('hello')
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="h-dvh w-screen bg-black p-5">
+      <Board />
+    </div>
+  );
 }
 
-export default App
+export default App;
